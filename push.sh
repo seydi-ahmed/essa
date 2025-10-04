a017d07e9eddf21e7238fc70c0fa805b8c22915b
seydi-ahmed <seydiahmedelcheikh@gmail.com>
2025-10-04 16:10:36 +0000

    initialisation des dossiers/fichiers du backend

--- push.sh
+++ push.sh
@@ -9,7 +9,7 @@ if ! git diff-index --quiet HEAD --; then
 fi
 
 # Pousser sur gitea et origin séparément, sortie silencieuse
-git push gitea main >/dev/null 2>&1
-git push github main >/dev/null 2>&1
+git push gitea >/dev/null 2>&1
+git push github >/dev/null 2>&1
 
 echo "✅ Push terminé sur gitea et github."
